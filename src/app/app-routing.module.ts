2ac80aca9eff83ab832909aaceda482dea354203
Prabeen <prabeensoreng2119@gmail.com>
2018-11-20 21:46:08 +0530

    auth module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { SigninComponent } from "./auth/signin/signin.component";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'signup', component: SignupComponent},
-    {path: 'signin', component: SigninComponent}
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- src/app/shopping-list/shopping-list-routing.module.ts
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import {  RouterModule, Routes } from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list.component";
 
-const shoppingListRouting: Routes = [
+const shoppingListRoutes: Routes = [
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(shoppingListRouting)],
+    imports: [RouterModule.forChild(shoppingListRoutes)],
     exports: [RouterModule]
 })
 export class ShoppingListRoutingModule {}
