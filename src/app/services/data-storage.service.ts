0a5fc5cf375fba71179ba134e23a079682bcb7ec
Prabeen <prabeensoreng2119@gmail.com>
2018-11-21 20:24:01 +0530

    http client added

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
 import { RecipeService } from './recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { map } from 'rxjs/operators';
@@ -12,13 +13,16 @@ export class DataStorageService {
 
   constructor(
     private recipeService: RecipeService,
-    private http: Http,
+    private httpClient: HttpClient,
     private authService: AuthService) { }
 
   storeRecipes() {
     const token = this.authService.getIdToken();
 
-    return this.http.put('https://ng-recipe-book-7281c.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes())
+    return this.httpClient.put('https://ng-recipe-book-7281c.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+      observe: 'body',
+      params: new HttpParams().set('auth', token)
+    })
       .subscribe(
         (response: Response) => {
           console.log(response);
@@ -29,10 +33,12 @@ export class DataStorageService {
   getRecipes() {
     const token = this.authService.getIdToken();
 
-    return this.http.get('https://ng-recipe-book-7281c.firebaseio.com/recipes.json?auth=' + token).pipe(
+    return this.httpClient.get<Recipe[]>('https://ng-recipe-book-7281c.firebaseio.com/recipes.json', {
+      observe: 'body',
+      params: new HttpParams().set('auth', token)
+    }).pipe(
       map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for(let recipe of recipes) {
             if(!recipe['ingredients'])
               recipe['ingredients'] = [];
